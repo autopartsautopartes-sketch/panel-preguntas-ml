7ee9159aaada86941b528c5aaa567ea0cb81db41
autopartsautopartes-sketch <autopartsautopartes@gmail.com>
2026-07-06 20:31:09 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 const crypto = require('crypto');
 const { URL } = require('url');
 // ==================== CONFIG ====================
